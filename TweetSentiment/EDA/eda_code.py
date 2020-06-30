2ef118b3ffc4db90bce39b6606831bcd894d690e
YOU POMIAN <53263496+Hanshawn11@users.noreply.github.com>
2020-06-30 17:16:55 +0900

    add word cloud codes

--- TweetSentiment/EDA/eda_code.py
+++ TweetSentiment/EDA/eda_code.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+from collections import Counter
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 #[1] find urls
 train = pd.read_csv('train.csv').fillna('')
@@ -42,3 +47,54 @@ new = train.groupby('sentiment')   # 分组， 分别统计各种情感的单词
 neutral = new.get_group('neutral').reset_index().describe()
 positive = new.get_group('positive').reset_index().describe()
 negative.get_group('negative').reset_index().describe()
+
+#[3] word cloud 制作
+stop_words = ['on','in', 'at', 'my', 'me', 'you', 'a', 'i', 'the', 'that', 'which', '2', 'm', 's', 'e', 'to', 'it', 'for', 'is', 'and', 'so', 't', 'of']
+def create_corpus(data,feature,sentiment):
+    #去除stop words 将出现的单词放在一个列表中
+    
+    corpus=[]
+    for x in data[data['sentiment']==sentiment][feature].str.split():
+        for i in x:
+            if i.lower() not in stop_words:
+                corpus.append(i.lower())
+    return corpus
+
+pos_words = Counter(create_corpus(train, 'target', 'positive'))   # 利用counter 构建词频字典
+pos_words_sorted  = sorted(pos_words.items(), key = lambda x:x[1], reverse=True)[:50]  # 排序并选取出现次数最多top 50 个单词
+
+negat_words = Counter(create_corpus(train, 'target', 'negative'))
+negat_words_sorted  = sorted(negat_words.items(), key = lambda x:x[1], reverse=True)[:50]  
+
+neutra_words = Counter(create_corpus(train, 'target', 'neutral'))
+neutra_words_sorted  = sorted(neutra_words.items(), key = lambda x:x[1], reverse=True)[:50]  
+
+png = 'twitt.png'  # <- 词云要填充的图案，我这里用推特小鸟
+fig = plt.gcf()
+fig.set_size_inches(16, 14)
+
+fig1 = plt.subplot(131)  # 1 行 3列 第 1张图
+data = {ele[0]:ele[1]  for ele in neutra_words_sorted}  #构建word：count 字典
+mask = np.array(Image.open(png))                        # img -> array, 词云的填充对象
+wc1 = WordCloud(background_color='white', mask=mask).generate_from_frequencies(data)
+plt.imshow(wc1)
+plt.title('neutral sentiment')
+plt.axis('off')            #关闭边框线
+
+fig2 = plt.subplot(132)
+data = {ele[0]:ele[1]  for ele in negat_words_sorted}
+mask = np.array(Image.open(png))
+wc1 = WordCloud(background_color='white', mask=mask).generate_from_frequencies(data)
+plt.imshow(wc1)
+plt.title('negative sentiment')
+plt.axis('off')
+
+fig3 = plt.subplot(133)
+data = {ele[0]:ele[1]  for ele in pos_words_sorted}
+mask = np.array(Image.open(png))
+wc1 = WordCloud(background_color='white', mask=mask).generate_from_frequencies(data)
+plt.imshow(wc1)   # 数字图像显示
+plt.title('positive sentiment')
+plt.axis('off')
+plt.savefig('words_cloud.png')
+plt.show()
